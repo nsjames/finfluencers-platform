eeb54878410cca0e2a818d0b86cec70e33e72949
Nathan James <skyandbeyond@gmail.com>
2021-04-01 23:41:28 +0300

    work

--- microservices/shared/services/Token.service.js
+++ microservices/shared/services/Token.service.js
@@ -25,11 +25,15 @@ const STALE_TIME = 60*60*4*1000;
 module.exports = class TokenService {
 
     static async getLastFetchTime(){
-       return ORM.get(`last_token_fetch_time`).catch(() => +new Date());
+       return ORM.get(`last_token_fetch_time`).catch(() => 0);
     }
 
     static async setLastFetchTime(){
         return ORM.upsert(+new Date(), `last_token_fetch_time`);
+    }
+
+    static async updateTokenPrices(startIndex = 1){
+        // TODO: Separate below and this so it doesn't hold up the UI.
     }
 
     static async cacheTokenList(startIndex = 1){
@@ -40,32 +44,34 @@ module.exports = class TokenService {
 	    // Only fetch once per day.
         const lastFetch = await this.getLastFetchTime();
 	    console.log('CACHING TOKEN LIST', lastFetch, (+new Date() - STALE_TIME))
-        if(lastFetch > (+new Date() - STALE_TIME)) return true;
+        if(lastFetch > 0 && lastFetch > (+new Date() - STALE_TIME)) return true;
 
 	    const date = +new Date();
 
         const results = await GET(`v1/cryptocurrency/listings/latest?start=${startIndex}&limit=${FETCH_LIMIT}&aux=platform&sort=volume_30d&sort_dir=desc`).then(x => x.data).catch(() => []);
         if(results.length){
-            await Promise.all(results.map(async data => {
-                const cachedToken = new CachedToken({
-                    id:data.id.toString(),
-                    name:data.name,
-                    symbol:data.symbol,
-                    platform:data.platform ? data.platform.symbol : data.symbol,
-                    contract:data.platform ? data.platform.token_address : '',
-                });
+            for(let i = 0; i < results.length; i++){
+                const data = results[i];
 
-                await ORM.upsert(cachedToken);
+	            const cachedToken = new CachedToken({
+		            id:data.id.toString(),
+		            name:data.name,
+		            symbol:data.symbol,
+		            platform:data.platform ? data.platform.symbol : data.symbol,
+		            contract:data.platform ? data.platform.token_address : '',
+	            });
+
+	            await ORM.upsert(cachedToken);
 
 
-                const tokenPrice = new TokenPrice({
-                    id:data.id.toString(),
-                    price:data.quote.USD.price.toString(),
-                    date
-                });
+	            const tokenPrice = new TokenPrice({
+		            id:data.id.toString(),
+		            price:data.quote.USD.price.toString(),
+		            date
+	            });
 
-                await ORM.upsert(tokenPrice);
-            }));
+	            await ORM.upsert(tokenPrice);
+            }
 
             // Don't need any more than this as we're sorting by volume_30d
             if(startIndex > FETCH_LIMIT*2) return finalize();
